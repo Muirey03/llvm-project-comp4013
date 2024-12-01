d14314bf7e61f3c7c9c068e649caa197deb74c59
Muirey03 <tcmuir03@gmail.com>
2024-12-01 16:25:47 +0000

    Begin checking retain-count semantics

--- clang/lib/StaticAnalyzer/Checkers/RacyUAFChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/RacyUAFChecker.cpp
@@ -5,6 +5,7 @@
 #include "clang/StaticAnalyzer/Core/PathSensitive/CallDescription.h"
 #include "clang/StaticAnalyzer/Core/PathSensitive/CallEvent.h"
 #include "clang/StaticAnalyzer/Core/PathSensitive/CheckerContext.h"
+#include "clang/Analysis/RetainSummaryManager.h"
 
 // DEBUG:
 #include "llvm/Support/Debug.h"
@@ -14,16 +15,34 @@ using namespace clang;
 using namespace ento;
 
 namespace {
+  class RefVal {
+  public:
+    RefVal(int cnt) : cnt(cnt) {}
+    int getCount() const { return cnt; }
+
+    RefVal operator+(int i) const {
+      return RefVal(getCount() + i);
+    }
+
+    RefVal operator-(int i) const {
+      return RefVal(getCount() - i);
+    }
+
+  protected:
+    int cnt = 0;
+  };
+
   class RacyUAFChecker : public Checker<check::PostCall> {
   public:
     void checkPostCall(const CallEvent &Call, CheckerContext &C) const;
 
   private:
+    // bug types:
     const BugType DoubleLockBugType{this, "Double locking"};
     const BugType DoubleUnlockBugType{this, "Double unlocking"};
 
+    // maps locking primitives to their respective handlers:
     typedef void (RacyUAFChecker::*FnCheck)(const CallEvent &Call, CheckerContext &C) const;
-
     CallDescriptionMap<FnCheck> PostCallHandlers = {
       {
         {CDM::CLibrary, {"pthread_mutex_lock"}, 1},
@@ -35,19 +54,48 @@ namespace {
       },
     };
 
-    void AcquireLock(const CallEvent &Call, CheckerContext &C) const;
+    // manager returns a "summary" about what effect a call should have on the receivers' retain counts
+    // TODO: subclass RetainSummaryManager to handle code annotations
+    mutable std::unique_ptr<RetainSummaryManager> Summaries;
 
+    RetainSummaryManager &getSummaryManager(ASTContext &Ctx) const {
+      // only track OSObjects for now:
+      if (!Summaries)
+        Summaries.reset(new RetainSummaryManager(Ctx, false, true));
+      return *Summaries;
+    }
+
+    RetainSummaryManager &getSummaryManager(CheckerContext &C) const {
+      return getSummaryManager(C.getASTContext());
+    }
+
+    // handle locking primitives:
+    void AcquireLock(const CallEvent &Call, CheckerContext &C) const;
     void ReleaseLock(const CallEvent &Call, CheckerContext &C) const;
 
+    // handle retain count manipulation:
+    void checkSummary(const RetainSummary &Summ, const CallEvent &Call, CheckerContext &C) const;
+
+    // helpers:
     void reportBug(CheckerContext &C, const BugType &bugType, const Expr *MtxExpr, StringRef Desc) const;
   };
 } // end anonymous namespace
 
 REGISTER_SET_WITH_PROGRAMSTATE(LockSet, const MemRegion *)
+REGISTER_MAP_WITH_PROGRAMSTATE(RefBindings, SymbolRef, RefVal)
 
 void RacyUAFChecker::checkPostCall(const CallEvent &Call, CheckerContext &C) const {
   if (const FnCheck *Callback = PostCallHandlers.lookup(Call))
     (this->**Callback)(Call, C);
+  else {
+    // not a locking primitive, check retain count semantics:
+    RetainSummaryManager &Summaries = getSummaryManager(C);
+    const Expr *CE = Call.getOriginExpr();
+    AnyCall anyCall = CE ? *AnyCall::forExpr(CE) : AnyCall(cast<CXXDestructorDecl>(Call.getDecl()));
+    QualType ReceiverType; // ReceiverType is exclusively for ObjC messages. We aren't tracking Obj-C, so leave it null
+    const RetainSummary *Summ = Summaries.getSummary(anyCall, Call.hasNonZeroCallbackArg(),false, ReceiverType);
+    checkSummary(*Summ, Call, C);
+  }
 }
 
 void RacyUAFChecker::AcquireLock(const CallEvent &Call, CheckerContext &C) const {
@@ -89,6 +137,10 @@ void RacyUAFChecker::ReleaseLock(const CallEvent &Call, CheckerContext &C) const
     state = state->remove<LockSet>(lockR);
     C.addTransition(state);
   }
+}
+
+void RacyUAFChecker::checkSummary(const RetainSummary &Summ, const CallEvent &Call, CheckerContext &C) const {
+  // TODO
 }
 
 void RacyUAFChecker::reportBug(CheckerContext &C, const BugType &bugType, const Expr *MtxExpr, StringRef Desc) const {
